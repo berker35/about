d388f12ddd2860e1e4f8622bc48d6d57ed8b07e2
berker35 <berker35@gmail.com>
2022-09-21 00:05:22 +0300

    V 2.2
    Implemented a better hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 import data from "./info.json" assert { type: "json" };
-
-$(".card,.get-menu ").click(function (e) {
+$(".toggle-menu").click(function () {
+  $(".menu").slideToggle();
+});
+$(".card, .menu-item").click(function (e) {
   $(".wrapper").fadeTo("fast", 0, function () {
     $(".overview-page").css({ display: "grid" });
     $(".overview-page").load("./info.html", function () {
@@ -12,22 +14,19 @@ $(".card,.get-menu ").click(function (e) {
       $(".revolution").html(data[e.currentTarget.id].revolution);
       $(".temp").html(data[e.currentTarget.id].temp);
       $(".radius").html(data[e.currentTarget.id].radius);
-      $(".wrapper").fadeTo("fast", 1, infoToggler());
-    });
-  });
-});
-
-function infoToggler() {
-  $(".planet").click(function (e) {
-    $(".wrapper").fadeTo("fast", 0, function () {
-      $(".overview-main > img").attr("src", data[e.currentTarget.id].image);
-      $(".overview-aside > h1").html(data[e.currentTarget.id].name);
-      $(".overview-aside > p").html(data[e.currentTarget.id].description);
-      $(".rotation").html(data[e.currentTarget.id].rotation);
-      $(".revolution").html(data[e.currentTarget.id].revolution);
-      $(".temp").html(data[e.currentTarget.id].temp);
-      $(".radius").html(data[e.currentTarget.id].radius);
       $(".wrapper").fadeTo("fast", 1);
     });
   });
-}
+});
+// $(".planet").click(function (e) {
+//   $(".wrapper").fadeTo("fast", 0, function () {
+//     $(".overview-main > img").attr("src", data[e.currentTarget.id].image);
+//     $(".overview-aside > h1").html(data[e.currentTarget.id].name);
+//     $(".overview-aside > p").html(data[e.currentTarget.id].description);
+//     $(".rotation").html(data[e.currentTarget.id].rotation);
+//     $(".revolution").html(data[e.currentTarget.id].revolution);
+//     $(".temp").html(data[e.currentTarget.id].temp);
+//     $(".radius").html(data[e.currentTarget.id].radius);
+//     $(".wrapper").fadeTo("fast", 1);
+//   });
+// });
